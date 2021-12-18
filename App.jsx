c6703ab35cb4cfdebf907f3efd6227b5c934c64c
sanhyokim <94211411+sanhyokim@users.noreply.github.com>
2021-12-18 13:57:25 +0900

    Create MemoEditScreen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
-import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
+    <MemoCreateScreen />
   );
 }
---
+++ src/components/icon.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import { createIconSetFromIcoMoon } from '@expo/vector-icons';
+// import { useFonts } from '@use-expo/font';
+
+// import icomoon from '../../assets/fonts/icomoon.ttf';
+// import section from '../../assets/fonts/selection.json';
+
+// export default function Icon(props) {
+//   const [fontLoded] = useFonts({ icomoon });
+//   const { name, size, color } = props;
+//   const CustomIcom = createIconSetFromIcoMoon(section);
+//   if (!fontLoded) {
+//     return null;
+//   }
+//   return <CustomIcom name={name} size={size} color={color} />;
+// }
